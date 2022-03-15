7eb426e010aeba06ecc5615150d31775a25c604b
Rajaditya Bajaj <rajbajaj200@gmail.com>
2022-03-15 16:57:41 -0400

    Initial commit

---
+++ app/src/main/java/edu/msu/bajajraj/puzzle/PuzzleActivity.java
@@ -0,0 +1,52 @@
+package edu.msu.bajajraj.puzzle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class PuzzleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_puzzle);
+
+        if(bundle != null) {
+            // We have saved state
+            getPuzzleView().loadInstanceState(bundle);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_puzzle, menu);
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+
+        getPuzzleView().saveInstanceState(bundle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_shuffle:
+                getPuzzleView().getPuzzle().shuffle();
+                getPuzzleView().invalidate();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }    }
+
+    private PuzzleView getPuzzleView() {
+        return (PuzzleView)this.findViewById(R.id.puzzleView);
+    }
+}
